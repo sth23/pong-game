833b4e03e552dbfbbb1e91a3375e8a4c0b3f287e
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-16 15:58:06 -0400

    Updated from Brython Server: 5/16/2019 3:58:05 PM

--- pong-game.py
+++ pong-game.py
@@ -31,7 +31,7 @@ class Ball(Sprite):
     def __init__(self, position):
         super().__init__(Ball.circ, position)
         self.fxcenter = self.fycenter = 0.5
-        self.speed = 5
+        self.speed = 10
         self.vx = self.speed
         self.vy = 0
         self.paddledistance = 0
